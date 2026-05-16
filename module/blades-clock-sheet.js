dd236f7fcc8bfd3c12e47f22e884c577373384f3
Karl Erik Hofseth <karl@kehofseth.no>
2026-05-16 22:39:10 +0200

    Refactor Clock sheet state mutation into the DataModel

--- module/blades-clock-sheet.js
+++ module/blades-clock-sheet.js
@@ -1,21 +1,10 @@
 import { BladesSheetV2 } from "./blades-sheet-v2.js";
-
-const CLOCK_COLORS = {
-  "black":  "BITD.Colors.Black",
-  "blue":   "BITD.Colors.Blue",
-  "green":  "BITD.Colors.Green",
-  "grey":   "BITD.Colors.Grey",
-  "red":    "BITD.Colors.Red",
-  "white":  "BITD.Colors.White",
-  "yellow": "BITD.Colors.Yellow"
-};
-
-const CLOCK_SIZES = { "4": "4", "6": "6", "8": "8", "10": "10", "12": "12" };
+import { ClockData } from "./data/clock.js";
 
 /**
- * Clock actor sheet. Picks an SVG asset from
- * {color, type, value} on every save and propagates the new texture to every
- * placed token of the actor on the current scene.
+ * Clock actor sheet. The SVG image recipe and the dropdown choices both
+ * live on ClockData; this class only handles rendering and the scene-level
+ * side effect of pushing the new texture to placed tokens.
  */
 export class BladesClockSheet extends BladesSheetV2 {
 
@@ -37,6 +26,10 @@ export class BladesClockSheet extends BladesSheetV2 {
 
   async _prepareContext(options) {
     const ctx = await super._prepareContext(options);
+    const schema = this.actor.system.schema.fields;
+    const sizeDropdown = Object.fromEntries(
+      schema.type.choices.map(n => [String(n), String(n)])
+    );
     return {
       ...ctx,
       actor: this.actor,
@@ -49,21 +42,24 @@ export class BladesClockSheet extends BladesSheetV2 {
       editable: this.isEditable,
       cssClass: this.isEditable ? "editable" : "locked",
       isGM: game.user.isGM,
-      sizeDropdown: CLOCK_SIZES,
-      colorDropdown: CLOCK_COLORS
+      sizeDropdown,
+      colorDropdown: schema.color.choices
     };
   }
 
-
-  // Updating the actor image and all tokens to the correct segments and fill level
+  // Bake the derived img / token texture into the same update as the form
+  // change, then sync placed tokens on the current scene.
   static async _onSubmitForm(event, form, formData) {
     const data = foundry.utils.expandObject(formData.object);
-    const color = data.system?.color ?? this.actor.system.color;
-    const type  = data.system?.type  ?? this.actor.system.type;
-    const value = data.system?.value ?? this.actor.system.value;
-    const imgPath = `systems/blades-in-the-dark/themes/${color}/${type}clock_${value}.svg`;
+    const sys = data.system ?? {};
+    const imgPath = ClockData.getImgPath({
+      color: sys.color ?? this.actor.system.color,
+      type:  sys.type  ?? this.actor.system.type,
+      value: sys.value ?? this.actor.system.value
+    });
     data.img = imgPath;
     foundry.utils.setProperty(data, "prototypeToken.texture.src", imgPath);
+    await this.actor.update(data);
 
     const scene = game.scenes.current;
     if (scene) {
@@ -73,7 +69,5 @@ export class BladesClockSheet extends BladesSheetV2 {
         await scene.updateEmbeddedDocuments("Token", tokenUpdates);
       }
     }
-
-    await this.actor.update(data);
   }
 }
--- module/data/clock.js
+++ module/data/clock.js
@@ -1,12 +1,24 @@
 const { fields } = foundry.data;
 
+const COLOR_CHOICES = {
+  black:  "BITD.Colors.Black",
+  blue:   "BITD.Colors.Blue",
+  green:  "BITD.Colors.Green",
+  grey:   "BITD.Colors.Grey",
+  red:    "BITD.Colors.Red",
+  white:  "BITD.Colors.White",
+  yellow: "BITD.Colors.Yellow"
+};
+
+const SIZE_CHOICES = [4, 6, 8, 10, 12];
+
 export class ClockData extends foundry.abstract.TypeDataModel {
   static defineSchema() {
     return {
       type: new fields.NumberField({
         required: true,
         initial: 4,
-        choices: [4, 6, 8, 10, 12],
+        choices: SIZE_CHOICES,
         integer: true
       }),
       value: new fields.NumberField({
@@ -18,8 +30,17 @@ export class ClockData extends foundry.abstract.TypeDataModel {
       color: new fields.StringField({
         required: true,
         initial: "black",
-        blank: false
+        blank: false,
+        choices: COLOR_CHOICES
       })
     };
+  }
+
+  static getImgPath({ color, type, value }) {
+    return `systems/blades-in-the-dark/themes/${color}/${type}clock_${value}.svg`;
+  }
+
+  prepareDerivedData() {
+    this.imgPath = ClockData.getImgPath(this);
   }
 }
